433e89314d5675b759bdafd5049488c2c6d95a59
Phili <philip-abangan@uiowa.edu>
2017-12-16 17:41:19 -0600

    Finished Code, debug exe file

--- animalRace/animalRace.cpp
+++ animalRace/animalRace.cpp
@@ -2,3 +2,44 @@
 // Created by Phili on 12/16/2017.
 //
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#include "simulation.h"
+
+int main(int argc, char *argv[])
+{
+    //Initializations
+    int currentWinner = TIE_CODE;
+    int timer=30000;
+    //The functions required a reference parameter, so I had to make pointers
+    //so that I could I access the pre-made functions.
+    int *winnerPtr = &currentWinner;
+    int *timePtr = &timer;
+    //This function should run until there is a winner.
+    HoldRace(winnerPtr, timePtr);
+    //Switch statement is used to print who the winner is.
+    switch(*winnerPtr)
+    {
+        case TORTOISE_CODE:
+            //TODO: Print message
+            cout << "TORTOISE WINS! YAY!!! " << endl;
+            break;
+        case HARE_CODE:
+            //TODO: Print message
+            cout << "Hare wins. Yuch. " << endl;
+            break;
+        case TIE_CODE:
+            //TODO: Print message
+            cout << "It's a tie. " << endl;
+            break;
+        default:
+            cout << "ERROR: Invalid winner found." << endl;
+    }
+    //Here I had to subtract from 30,000 because it was given to us as that.
+    //TODO:  Print time elapsed information
+    cout << "TIME ELAPSED: " << 30000-*timePtr << " seconds" << endl;
+    return EXIT_SUCCESS;
+}
--- animalRace/simulation.cpp
+++ animalRace/simulation.cpp
@@ -10,3 +10,127 @@ int GetRandomInteger()
     static std::uniform_int_distribution<int> randomInt{1, 10};
     return randomInt(engine);
 }
+
+void moveTortoise(int *turtlePtr, int randomNumber)
+{
+    int i = randomNumber;
+    if((1<=i) && (i<=5))
+    {
+        *turtlePtr += 3;
+    }
+    else if((6 <= i) && (i <= 7))
+    {
+        *turtlePtr -= 6;
+        if (*turtlePtr < 1)
+        {
+            *turtlePtr = 1;
+        }
+    }
+    else if ((8 <= i) && (i <= 10))
+    {
+        *turtlePtr += 1;
+    }
+}
+
+void moveHare(int * rabbitPtr,  int randomNumber)
+{
+    int i = randomNumber;
+    string move = "";
+    if ((1 <= i) && (i <= 2))
+    {
+        move = "Sleep";
+    }
+    if ((3 <= i) && (i <= 4))
+    {
+        *rabbitPtr += 9;
+        move = "Big Hop";
+    }
+    if (i == 5)
+    {
+        *rabbitPtr -= 12;
+        move = "Big Slip";
+        if (*rabbitPtr < 1)
+        {
+            *rabbitPtr = 1;
+        }
+    }
+    if ((6 <= i) && (i <= 8))
+    {
+        *rabbitPtr += 1;
+        move = "Small Hop";
+    }
+    if ((9 <= i) && (i <= 10))
+    {
+        *rabbitPtr -= 2;
+        move = "Small slip";
+        if (*rabbitPtr < 1)
+        {
+            *rabbitPtr = 1;
+        }
+    }
+}
+
+string CreateEmptyRacetrack(const unsigned int len)
+{
+    string Track = "|";
+    for (unsigned int i=0; i<len; i++)
+    {
+        Track = Track + ".";
+    }
+    Track = Track + "|";
+    return std::string (Track);
+}
+
+std::string drawStringOfCurrentPosition(const int * snapperPtr,
+                                        const int * bunnyPtr)
+{
+    const string ouch("@");
+    const string bunnyName("H");
+    const string turtleName("T");
+
+    string emptyRace = CreateEmptyRacetrack(RACE_DISTANCE);
+
+    if (*snapperPtr == *bunnyPtr)
+    {
+        emptyRace[*snapperPtr] = ouch[0];
+    }
+    else
+    {
+        emptyRace[*snapperPtr] = turtleName[0];
+        emptyRace[*bunnyPtr] = bunnyName[0];
+    }
+    return emptyRace;
+}
+
+std::string HoldRace(int *winner, int *timer)
+{
+    stringstream display;
+    display << "ON YOUR MARK, GET SET\nBANG               !!!!"
+            << "\nAND THEY'RE OFF    !!!!\n";
+
+    int turtlePosition = 1;
+    int harePosion = 1;
+    int * turtlePtr = &turtlePosition;
+    int * rabbitPtr = &harePosion;
+    while ((* turtlePtr < RACE_DISTANCE) && (* rabbitPtr < RACE_DISTANCE))
+    {
+        --*timer;
+        moveTortoise(turtlePtr, GetRandomInteger());
+        moveHare(rabbitPtr, GetRandomInteger());
+        cout << drawStringOfCurrentPosition(turtlePtr, rabbitPtr) << endl;
+    }
+    if (*turtlePtr > *rabbitPtr)
+    {
+        *winner = TORTOISE_CODE;
+    }
+    else if (*rabbitPtr > *turtlePtr)
+    {
+        *winner = HARE_CODE;
+    }
+    else
+    {
+        *winner = TIE_CODE;
+    }
+
+    return display.str();
+}
